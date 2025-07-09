02d5c645360d4867c8b33d371f4a2f98b4fa44bc
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-07-09 15:07:36 +0330

    Change folder structure and setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './routes/App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const routes = [{ path: '/', element: <App /> }];
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ src/routes/App.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledH1 = styled.h1`
+  background-color: burlywood;
+  font-size: 3rem;
+`;
+
+function App() {
+  return <StyledH1>Hello World!</StyledH1>;
+}
+
+export default App;
