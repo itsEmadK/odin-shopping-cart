b9db6203fb161d67c506ab0c0d7f998da836f336
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-07-15 19:17:36 +0330

    Add padding for outlet

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import styled from 'styled-components';
+
+const OutletWrapper = styled.div`
+  padding: 1rem;
+`;
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <OutletWrapper>
+        <Outlet />
+      </OutletWrapper>
     </>
   );
 }
