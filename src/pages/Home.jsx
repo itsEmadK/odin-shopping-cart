238fbf867ec4b49ec7c96b670c2eba9f85bb2f2c
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-07-22 19:54:45 +0330

    Add home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,38 @@
+import styled from 'styled-components';
+import theme from '../util/theme.json';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  display: grid;
+  place-content: center;
+  gap: 1rem;
+  justify-items: center;
+`;
+
+const Title = styled.h2`
+  color: ${theme.schemes.light.onBackground};
+`;
+
+const StartButton = styled(Link)`
+  background-color: ${theme.schemes.light.primaryContainer};
+  color: ${theme.schemes.light.onPrimaryContainer};
+  border: 2px solid ${theme.schemes.light.onPrimaryContainer};
+  padding: 4px 8px;
+  font-size: 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover {
+    background-color: ${theme.schemes.light.prim};
+  }
+  text-decoration: none;
+`;
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <Title>Enter another galaxy by shopping from us!</Title>
+      <StartButton to={'products'}>Start Shopping</StartButton>
+    </Container>
+  );
 }
