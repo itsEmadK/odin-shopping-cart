7b004093b4bb2c12f1f798e7a17176e2ac660204
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-07-23 20:24:28 +0330

    Finish up products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,45 @@
-export default function Products() {
-  return <h1>Products</h1>;
+import { useOutletContext } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
+import styled from 'styled-components';
+
+const Heading = styled.h2`
+  margin-bottom: 1rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
+  gap: 1rem;
+`;
+
+export default function Products({
+  onProductAddToCart,
+  onProductRemoveFromCart,
+}) {
+  const { loading, products, error, cart } = useOutletContext();
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  if (error) {
+    return error.toString();
+  }
+
+  return (
+    <>
+      <Heading>Products</Heading>
+      <Grid>
+        {products.map((p) => (
+          <ProductCard
+            key={p.id}
+            product={p}
+            onAddToCart={() => onProductAddToCart(p.id)}
+            onRemoveFromCart={() => onProductRemoveFromCart(p.id)}
+            count={cart[p.id] ? cart[p.id] : 0}
+          />
+        ))}
+      </Grid>
+    </>
+  );
 }
