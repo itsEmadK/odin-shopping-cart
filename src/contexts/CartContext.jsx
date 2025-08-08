a88fb03bcbee10864407fe9a6fae94c52a107b29
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-08-08 14:53:20 +0330

    Use reducer instead of state

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useMemo, useReducer } from 'react';
 
 // const CartContext = createContext();
 const CartDataContext = createContext();
@@ -8,12 +8,58 @@ export const useCartData = () => useContext(CartDataContext);
 export const useCartApi = () => useContext(CartApiContext);
 
 export default function CartContextProvider({ children }) {
-  const [cart, setCart] = useState({});
+  const [cart, dispatch] = useReducer(cartReducer, {});
+  const api = useMemo(
+    () => ({
+      addToCart: (productId) => {
+        dispatch({ type: 'addProduct', productId });
+      },
+      removeFromCart: (productId) => {
+        dispatch({ type: 'removeProduct', productId });
+      },
+      purchase: () => {
+        dispatch({ type: 'purchase' });
+      },
+    }),
+    []
+  );
   return (
     <CartDataContext.Provider value={cart}>
-      <CartApiContext.Provider value={setCart}>
+      <CartApiContext.Provider value={api}>
         {children}
       </CartApiContext.Provider>
     </CartDataContext.Provider>
   );
+}
+
+function cartReducer(state, action) {
+  switch (action.type) {
+    case 'addProduct': {
+      const newCart = { ...state };
+      if (newCart[action.productId]) {
+        newCart[action.productId] = newCart[action.productId] + 1;
+      } else {
+        newCart[action.productId] = 1;
+      }
+      return newCart;
+    }
+    case 'removeProduct': {
+      const newCart = { ...state };
+      if (newCart[action.productId]) {
+        if (newCart[action.productId] === 1) {
+          newCart[action.productId] = undefined;
+        } else {
+          newCart[action.productId] = newCart[action.productId] - 1;
+        }
+      }
+      return newCart;
+    }
+    case 'purchase': {
+      const newCart = {};
+      return newCart;
+    }
+
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
 }
