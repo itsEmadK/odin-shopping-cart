d88607ac7bf7b878e2c5ac21df95983b1bc6a5e9
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-08-08 15:29:58 +0330

    Replace the mock api with the real one

--- src/contexts/ProductsContext.jsx
+++ src/contexts/ProductsContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from 'react';
-import useMockedProductsFetchData from '../mocks/useMockedProductsFetchData';
+import useProductsFetchData from '../hooks/useProductsFetchData.js';
 
 const ProductsContext = createContext();
 
 export const useProducts = () => useContext(ProductsContext);
 
 export default function ProductsContextProvider({ children }) {
-  const { loading, error, products } = useMockedProductsFetchData();
+  const { loading, error, products } = useProductsFetchData();
   return (
     <ProductsContext.Provider value={{ loading, error, products }}>
       {children}
